a56311a15d732eea1ce6406e0c9f54c8d33b6afb
Runqi Han <77292603+RunQi-Han@users.noreply.github.com>
2024-09-24 16:03:47 -0400

    Add files via upload

---
+++ runqi-deploy-1/app.py
@@ -0,0 +1,33 @@
+from dash import Dash, dcc
+import dash_design_kit as ddk
+import plotly.express as px
+import datetime
+import pytz
+
+
+
+# Get the current time in UTC 23
+utc_time = datetime.datetime.now(pytz.utc)
+# Define the EST timezone
+est = pytz.timezone('US/Eastern')
+
+app = Dash(__name__)
+server = app.server  # expose server variable for Procfile
+
+
+df = px.data.stocks()
+
+app.layout = ddk.App([
+
+    ddk.Header([
+        ddk.Logo(src=app.get_asset_url('logo.svg')),
+        ddk.Title('The APP 111 deployed time is: ' + str(utc_time.astimezone(est))),
+    ]),
+
+    ddk.Card(children=ddk.Graph(figure=px.line(df, x="date", y=["GOOG", "AAPL"], title='Stock Prices')))
+
+])
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
